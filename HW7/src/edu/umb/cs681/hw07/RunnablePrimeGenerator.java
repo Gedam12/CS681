77ab05c9da3cb91c11365e444fe4210a674520e9
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-10-30 08:37:27 -0400

    Update RunnablePrimeGenerator.java

--- HW7/src/edu/umb/cs681/hw07/RunnablePrimeGenerator.java
+++ HW7/src/edu/umb/cs681/hw07/RunnablePrimeGenerator.java
@@ -9,10 +9,10 @@ public class RunnablePrimeGenerator extends PrimeGenerator implements Runnable {
 	public void run() {
 		generatePrimes();
 	}
-
-	public static void main(String[] args) {
-		RunnablePrimeGenerator gen1 = new RunnablePrimeGenerator(1, 100);
-		RunnablePrimeGenerator gen2 = new RunnablePrimeGenerator(101, 200);
+	
+	public void generateDoubleThread() {
+		RunnablePrimeGenerator gen1 = new RunnablePrimeGenerator(1, 10000000);
+		RunnablePrimeGenerator gen2 = new RunnablePrimeGenerator(10000001, 20000000);
 		Thread t1 = new Thread(gen1);
 		Thread t2 = new Thread(gen2);
 		t1.start();
@@ -27,8 +27,79 @@ public class RunnablePrimeGenerator extends PrimeGenerator implements Runnable {
 		
 		long primeNum = gen1.getPrimes().size() + gen2.getPrimes().size();
 		System.out.println("\n" + primeNum + " prime numbers are found in total.");
-        
+	}
+	
+	public void generateSingleThread() {
+		RunnablePrimeGenerator gen1 = new RunnablePrimeGenerator(1, 10000000);
+		Thread t1 = new Thread(gen1);
+		t1.start();
+		try {
+			t1.join();
+		} catch (InterruptedException e) {}
 
+		gen1.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		long primeNum = gen1.getPrimes().size();
+		System.out.println("\n" + primeNum + " prime numbers are found in total.");
+        
+	}
+	
+	public void generateDoubleThread() {
+		RunnablePrimeGenerator gen1 = new RunnablePrimeGenerator(1, 10000000);
+		RunnablePrimeGenerator gen2 = new RunnablePrimeGenerator(10000001, 20000000);
+		Thread t1 = new Thread(gen1);
+		Thread t2 = new Thread(gen2);
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {}
+
+		gen1.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		gen2.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		
+		long primeNum = gen1.getPrimes().size() + gen2.getPrimes().size();
+		System.out.println("\n" + primeNum + " prime numbers are found in total.");
+	}
+	
+	public void generateQuadThread() {
+		RunnablePrimeGenerator gen1 = new RunnablePrimeGenerator(1, 10000000);
+		RunnablePrimeGenerator gen2 = new RunnablePrimeGenerator(10000001, 20000000);
+		RunnablePrimeGenerator gen3 = new RunnablePrimeGenerator(20000001, 30000000);
+		RunnablePrimeGenerator gen4 = new RunnablePrimeGenerator(30000001, 40000000);
+		Thread t1 = new Thread(gen1);
+		Thread t2 = new Thread(gen2);
+		Thread t3 = new Thread(gen3);
+		Thread t4 = new Thread(gen4);
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+			t4.join();
+		} catch (InterruptedException e) {}
+
+		gen1.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		gen2.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		gen3.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		gen4.getPrimes().forEach( (Long prime)->System.out.print(prime + ", ") );
+		
+		long primeNum = gen1.getPrimes().size() + gen2.getPrimes().size() + gen3.getPrimes().size() + gen4.getPrimes().size();
+		System.out.println("\n" + primeNum + " prime numbers are found in total.");
+	}
+	
+
+	public static void main(String[] args) {
+		
+		generateSingleThread();
+		
+		generateDoubleThread();
+		
+		generateQuadThread();
+		
 	}
 
 }
