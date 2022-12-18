7d1d257f86a1e3c063759cc2ab449b7a5b75d2f3
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-18 11:35:02 -0500

    Create EntranceHandler.java

---
+++ HW16/scr/edu/umb/cs681/EntranceHandler.java
@@ -0,0 +1,38 @@
+package edu.umb.cs681.hw16;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.swing.InputVerifier;
+
+import java.util.concurrent.locks.Condition;
+
+public class EntranceHandler implements Runnable {
+
+	private AdmissionMonitor monitor = new AdmissionMonitor();
+	private boolean done = false;
+	private final ReentrantLock lock = new ReentrantLock();
+	Condition overflowCondition = lock.newCondition();
+
+	public EntranceHandler() {}
+
+	public void setDone(){
+		lock.lock();
+		try {
+			done = true;
+		}
+		finally {
+			lock.unlock();
+		}
+	}
+	
+public void run() {
+		lock.lock();
+		try { 
+			if (done) {
+				System.out.println("Stopped monitoring entrances...");
+			}
+			monitor.enter();
+		} finally {
+		lock.unlock();
+		}
+	}
+}
