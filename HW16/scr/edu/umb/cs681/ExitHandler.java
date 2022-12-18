0ebc31dd87907fec41e9033a2bd761dda0bf6cdd
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-18 11:37:51 -0500

    Create ExitHandler.java

---
+++ HW16/scr/edu/umb/cs681/ExitHandler.java
@@ -0,0 +1,36 @@
+package edu.umb.cs681.hw16;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+public class ExitHandler implements Runnable {
+
+	private AdmissionMonitor monitor = new AdmissionMonitor();
+	private boolean done = false;
+	private final ReentrantLock lock = new ReentrantLock();
+	Condition overflowCondition = lock.newCondition();
+	
+	public ExitHandler() {}
+
+	public void setDone(){
+		lock.lock();
+		try {
+			done = true;
+		}
+		finally {
+			lock.unlock();
+		}
+	}
+	
+	public void run() {
+		lock.lock();
+		try { 
+			if (done) {
+				System.out.println("Stopped monitoring exits...");
+			}
+			monitor.exit();
+		} finally {
+			lock.unlock();
+		}
+	}
+}
