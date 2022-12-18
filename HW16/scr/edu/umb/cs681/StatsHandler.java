fbbc767313f9a1119bd451bd1994970fe6d15759
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-18 11:38:52 -0500

    Create StatsHandler.java

---
+++ HW16/scr/edu/umb/cs681/StatsHandler.java
@@ -0,0 +1,36 @@
+package edu.umb.cs681.hw16;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+public class StatsHandler implements Runnable {
+
+	private AdmissionMonitor monitor = new AdmissionMonitor();
+	private boolean done = false;
+	private final ReentrantLock lock = new ReentrantLock();
+	Condition condition = lock.newCondition();
+	
+	public StatsHandler() {}
+
+	public void setDone(){
+		lock.lock();
+		try {
+			done = true;
+		}
+		finally {
+			lock.unlock();
+		}
+	}
+	
+	public void run() {
+		lock.lock();
+		try { 
+			if (done) {
+				System.out.println("Stopped monitoring stats...");
+			}
+			monitor.countCurrentVisitors();
+		} finally {
+		lock.unlock();
+		}
+	}
+}
