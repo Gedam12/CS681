d7194d1af94b02eab4e728d540e22fb5af53f4c0
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-19 14:05:43 -0500

    Create RequestHandler.java

---
+++ HW13/src/edu/umb/cs681/hw13/RequestHandler.java
@@ -0,0 +1,18 @@
+package edu.umb.cs681.hw13;
+
+import java.nio.file.*;
+
+public class RequestHandler implements Runnable {
+	
+	private AccessCounter ac;
+	private Path path;
+	
+	public RequestHandler(AccessCounter ac, Path p) {
+		this.ac = ac;
+		this.path = p;
+	}    
+    public void run(){
+		ac.increment(path);
+		ac.getCount(path);
+	}
+}
