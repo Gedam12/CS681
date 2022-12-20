3b112af76843219d904ad8c61360adb5ed61bf38
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-19 20:24:16 -0500

    Add files via upload

--- src/edu/umb/cs681/hw19/ConcurrentLinkedQueue.java
+++ src/edu/umb/cs681/hw19/ConcurrentLinkedQueue.java
@@ -1,1 +1,27 @@
+package edu.umb.cs681.hw19;
 
+import java.util.concurrent.*;
+ 
+class ConcurrentLinkedQueue {
+   
+    public static void main(String[] args)
+    {
+        ConcurrentLinkedQueue<Integer>
+            clq = new ConcurrentLinkedQueue<Integer>();
+ 
+        clq.add(12);
+        clq.add(70);
+        clq.add(1009);
+        clq.add(475);
+ 
+        // Displaying the existing LinkedQueue
+        System.out.println("ConcurrentLinkedQueue: "
+                           + clq);
+ 
+        ConcurrentLinkedQueue<Integer>
+            clq1 = new ConcurrentLinkedQueue<Integer>(clq);
+ 
+        System.out.println("ConcurrentLinkedQueue1: "
+                           + clq1);
+    }
+}
