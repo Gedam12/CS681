60daf6d58abdb0dde69daadceae0e383e6f21772
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-19 19:06:36 -0500

    Add files via upload

--- HW10/src/edu/umb/cs681/hw10/RunnablePrimeFactorizer.java
+++ HW10/src/edu/umb/cs681/hw10/RunnablePrimeFactorizer.java
@@ -1,21 +1,10 @@
-package edu.umb.cs681.hw10;
+//package edu.umb.cs681.hw10;
 
 import java.util.LinkedList;
 
-// Generates prime factors of a given number (dividend)
-// Factorization is performed in the range of 2 and Math.sqrt(dividend)
-//
-// When only one thread is used for factorization, create a Runnable as follows:
-//     new RunnablePrimeFactorizer(dividend, 2, (long)Math.sqrt(dividend));
-// The thread performs factorization for the entire range of 2 and sqrt(dividend). 
-//
-// When two threads are used for factorization, create two Runnables as follows:
-//     new RunnablePrimeFactorizer(dividend, 2, (long)Math.sqrt(dividend)/2);
-//     new RunnablePrimeFactorizer(dividend, 1+(long)Math.sqrt(dividend)/2, (long)Math.sqrt(dividend));
-
-public class RunnablePrimeFactorizer extends PrimeFactorizer implements Runnable {
+public class Runnable PrimeFactorizer extends PrimeFactorizer implements Runnable {
 	
-	public RunnablePrimeFactorizer(long dividend, long from, long to) {
+	public Runnable PrimeFactorizer(long dividend, long from, long to) {
 		super(dividend);
 		if (from >= 2 && to >= from) {
 			this.from = from;
