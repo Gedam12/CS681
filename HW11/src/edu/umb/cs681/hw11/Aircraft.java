137687eaa6a77ef5e6cc2edd767f57738c3c3c1a
sayaligedam <91810513+Gedam12@users.noreply.github.com>
2022-12-19 19:56:33 -0500

    Add files via upload

--- HW11/src/edu/umb/cs681/hw11/Aircraft.java
+++ HW11/src/edu/umb/cs681/hw11/Aircraft.java
@@ -3,14 +3,14 @@ package edu.umb.cs681.hw11;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Aircraft {
-	private AtomicReference<Position> position; // Shared (non-final) variable
+	private AtomicReference <pos> position;
 	public Aircraft(Position pos){ 
 		position = new AtomicReference<>(pos); 
 	}
 	public void setPosition(Position pos){
 		position.set(pos);;
 	}
-	public Position getPosition(){ // 2 steps; NOT thread-safe.
+	public Position getPosition(){ 
 		return this.position.get();
 	} 
 }
